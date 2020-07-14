66a2bff3d636c69851a88d239c9dcc7d74cd7aee
fan <610398596@qq.com>
2020-07-14 18:35:38 +0800

    测试学生需求

--- src/test/java/com/thoughtworks/basic/ClassTest.java
+++ src/test/java/com/thoughtworks/basic/ClassTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.basic;
 
 import com.thoughtworks.basic.entity.Person;
+import com.thoughtworks.basic.entity.Student;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -17,5 +18,15 @@ public class ClassTest {
         //then
         assertEquals(actual,"My name is Tome. I am 21 years old.");
     }
+    @Test
+    public void student_test() {
+        //given
+        Student student=new Student();
 
+        //when
+        String actual =student.introduce("Tom","21","2");
+
+        //then
+        assertEquals(actual,"My name is Tom. I am 21 years old. I am a Student of Class 2.");
+    }
 }
