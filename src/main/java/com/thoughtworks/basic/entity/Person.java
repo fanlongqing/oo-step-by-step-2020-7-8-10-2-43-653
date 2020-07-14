b586428a9a85a53ff9861019794a6e5bf79e6a9e
fan <610398596@qq.com>
2020-07-14 17:52:56 +0800

    新建实体类person

---
+++ src/main/java/com/thoughtworks/basic/entity/Person.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.basic.entity;
+
+public class Person {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String introduce(String name,String age){
+        return "My name is" +name+". I am"+ age+"years old." ;
+
+    }
+}
